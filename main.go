3a5cb2cd00c4b6dd4912da7bf7eb8e65185845fb
hbk01 <3243430237@qq.com>
2020-10-05 19:19:01 +0800

    init appid and appkey

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+)
+
+const (
+	ID        = "3cd47726b488d5ad"
+	KEY       = "WiAQEJoYTzUcF9We6HnwkFAy7NByYfMt"
+	LANG_AUTO = "auto"
+	LANG_EN   = "en"
+	LANG_CN   = "zh-CHS"
 )
 
 func main() {
-	fmt.Println("")
+	fmt.Println(os.Args[1])
 }
