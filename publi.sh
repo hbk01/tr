2f2a05ecd3b3b282eba9aea5769a1445d8510e30
hbk01 <3243430237@qq.com>
2020-10-07 12:19:32 +0800

    build linux,macOS,windows binary files

--- publi.sh
+++ publi.sh
@@ -11,9 +11,41 @@ if [ ! $1 ]; then
     exit 1
 fi
 
-echo "- clean and build"
+echo "- clean"
 rm -rf ./bin
-go build -o ./bin/tr
+
+echo "- build binary files"
+os=linux
+echo "  - build $os"
+echo "    - build amd64"
+GOOS=$os GOARCH=amd64 go build -o ./bin/tr_$1-$os-amd64
+echo "    - build 386"
+GOOS=$os GOARCH=386 go build -o ./bin/tr_$1-$os-386
+echo "    - build arm"
+GOOS=$os GOARCH=arm go build -o ./bin/tr_$1-$os-arm
+
+os=freebsd
+echo "  - build $os"
+echo "    - build amd64"
+GOOS=$os GOARCH=amd64 go build -o ./bin/tr_$1-$os-amd64
+echo "    - build 386"
+GOOS=$os GOARCH=386 go build -o ./bin/tr_$1-$os-386
+echo "    - build arm"
+GOOS=$os GOARCH=arm go build -o ./bin/tr_$1-$os-arm
+
+os=darwin
+echo "  - build $os"
+echo "    - build amd64"
+GOOS=$os GOARCH=amd64 go build -o ./bin/tr_$1-macOS-amd64
+
+os=windows
+echo "  - build $os"
+echo "    - build x64"
+GOOS=$os GOARCH=amd64 go build -o ./bin/tr_$1-$os-x64.exe
+echo "    - build x86"
+GOOS=$os GOARCH=386 go build -o ./bin/tr_$1-$os-x86.exe
+
+echo "- build finished."
 
 if [ $? -ne 0 ]; then
     echo "Build Failed."
@@ -28,13 +60,20 @@ if [ $? -ne 0 ]; then
     gh auth login
 fi
 
+echo "files will be upload to assets: "
+files=$(ls ./bin)
+for file in $files
+do
+    echo "  $file"
+done
+
 echo "- create tag $1"
 git tag $1 -m "Release $1"
 echo "- pushing tags"
 git push --tags
 
 echo "- release as draft..."
-gh release create $1 --draft --title "Release $1" --notes-file ./release.note ./bin/tr
+gh release create $1 --draft --title "Release $1" --notes-file ./release.note ./bin/tr*
 
 if [ $? -eq 0 ]; then
     echo "release sussess, please view https://github.com/hbk01/tr/releases to comfirm and publish!"
